987f1b2ba86cdbd7b7ec341881f077d73d9eebe7
Stuart Raynor <madstu@gmail.com>
2018-04-18 09:29:56 +0100

    Add files via upload

--- newtzocmn.sh
+++ newtzocmn.sh
@@ -1,45 +1,39 @@
-mkdir zeroonetest
-cd zeroonetest
-wget https://github.com/MadStu/tZOC/raw/master/zerooned
-wget https://github.com/MadStu/tZOC/raw/master/zeroone-cli
-chmod 777 zerooned
-chmod 777 zeroone-cli
-mkdir ../.zeroonecore
-EXIP=$(curl ipinfo.io/ip)
-printf "rpcuser=yesme\nrpcpassword=f5hanfsdfia\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndeamon=1\nlogtimestamps=1\nmaxconnections=256\ntestnet=1\n\nexternalip=$EXIP\nbind=$EXIP:10001\n\naddnode=94.177.214.132:10001\n\n" > /$HOME/.zeroonecore/zeroone.conf
-./zerooned --daemon --datadir=$HOME/.zeroonecore
-sleep 10
-MKEY=$(./zeroone-cli masternode genkey)
-./zeroone-cli stop
-echo -e "masternode=1\nmasternodeprivkey=$MKEY\n\n" >> /$HOME/.zeroonecore/zeroone.conf
-sleep 30
-./zerooned --daemon --datadir=$HOME/.zeroonecore
-
-
-
-
-
 clear
-echo "==============================="
-echo "==                           =="
-echo "==  MadStu's Reindex Script  =="
-echo "==                           =="
-echo "==============================="
-echo " "
+sleep 1
 if [ -e getinfo.json ]
 then
 	echo "Script running already"
 else
+
+#sudo apt-get install jq -y
+
+killall zerooned
+rm -rf zero*
+rm -rf .zero*
+wget https://github.com/zocteam/zeroonecoin/releases/download/V0.12.1.6/zeroone-linux.tar.gz
+tar -xvzf zeroone-linux.tar.gz
+
+
+
+mkdir ~/.zeroonecore
+EXIP=$(curl ipinfo.io/ip)
+printf "rpcuser=yesme\nrpcpassword=f5hanfsdfia\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndeamon=1\nlogtimestamps=1\nmaxconnections=64\ntestnet=1\ngen=0\n\nexternalip=$EXIP\nbind=$EXIP:10001\n\naddnode=01coin.io\n\n" > ~/.zeroonecore/zeroone.conf
+~/zeroone/zerooned -daemon
+sleep 10
+MKEY=$(~/zeroone/zeroone-cli masternode genkey)
+~/zeroone/zeroone-cli stop
+echo -e "masternode=1\nmasternodeprivkey=$MKEY\n\n" >> ~/.zeroonecore/zeroone.conf
+sleep 10
+~/zeroone/zerooned -daemon
+
+
+
+
+
+
 echo "blah" > getinfo.json
-./zeroone-cli stop
-sleep 5
-cd ~/.zeroonecore
-rm mncache.dat
-rm mnpayments.dat
-cd ~/zeroonetest
-./zerooned -daemon -reindex
 sleep 2
-ARRAY=$(./zeroone-cli getinfo)
+ARRAY=$(~/zeroone/zeroone-cli getinfo)
 echo "$ARRAY" > getinfo.json
 BLOCKCOUNT=$(curl https://testexplorer.01coin.io/api/getblockcount)
 WALLETBLOCKS=$(jq '.blocks' getinfo.json)
@@ -58,9 +52,9 @@ while [ "$menu" != 1 ]; do
 			echo " "
 			echo "  Blocks required: $BLOCKCOUNT"
 			echo "    Blocks so far: $WALLETBLOCKS"
-			sleep 60
+			sleep 20
 			BLOCKCOUNT=$(curl https://testexplorer.01coin.io/api/getblockcount)
-			ARRAY=$(./zeroone-cli getinfo)
+			ARRAY=$(~/zeroone/zeroone-cli getinfo)
 			echo "$ARRAY" > getinfo.json
 			WALLETBLOCKS=$(jq '.blocks' getinfo.json)
 			;;
@@ -68,7 +62,7 @@ while [ "$menu" != 1 ]; do
 done
 echo "Now wait for AssetID: 999..."
 sleep 3
-MNSYNC=$(./zeroone-cli mnsync status)
+MNSYNC=$(~/zeroone/zeroone-cli mnsync status)
 echo "$MNSYNC" > mnsync.json
 ASSETID=$(jq '.AssetID' mnsync.json)
 echo "Current Asset ID: $ASSETID"
@@ -94,7 +88,7 @@ while [ "$meanu" != 1 ]; do
 			echo " "
 			echo "  AssetID: $ASSETID"
 			sleep 10
-			MNSYNC=$(./zeroone-cli mnsync status)
+			MNSYNC=$(~/zeroone/zeroone-cli mnsync status)
 			echo "$MNSYNC" > mnsync.json
 			ASSETID=$(jq '.AssetID' mnsync.json)
 			;;
@@ -103,7 +97,7 @@ done
 rm mnsync.json
 echo " "
 echo " "
-./zeroone-cli mnsync status
+~/zeroone/zeroone-cli mnsync status
 echo " "
 echo " "
 echo "  You can now Start Alias in the windows wallet!"
